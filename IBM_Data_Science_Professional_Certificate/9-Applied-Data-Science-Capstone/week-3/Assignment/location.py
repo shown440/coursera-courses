6b05e70619b8a0610545ebe00c00bb9aaa93a06a
shown440 <ahmed.shifullah@gmail.com>
2020-04-29 20:57:50 +0600

    9.Capstone week 3 completed

---
+++ IBM_Data_Science_Professional_Certificate/9-Applied-Data-Science-Capstone/week-3/Assignment/location.py
@@ -0,0 +1,13 @@
+import geocoder
+g = geocoder.google('Mountain View, CA')
+print(g.latlng)
+
+import requests
+import json
+
+send_url = 'http://freegeoip.net/json'
+r = requests.get(send_url)
+j = json.loads(r.text)
+lat = j['latitude']
+lon = j['longitude']
+print(lat, lon)
